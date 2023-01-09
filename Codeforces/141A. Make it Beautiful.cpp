39ee1762d927969222b8a4778f8593dd80eb78ce
SajalRuhela017 <ruhela.sajal017@gmail.com>
2023-01-09 22:44:22 +0530

    First Commit

---
+++ Codeforces/141A. Make it Beautiful.cpp
@@ -0,0 +1,58 @@
+/**
+ *      author : rhsajal_017
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t, k = 1;
+    cin >> t;
+    while(t--)
+    {
+        long long n, i, j, sm, la, m, c;
+        cin>>n;
+        m = n * n;
+        sm = 1;
+        la = m;
+        long long ans[n + 1][n + 1];
+        c = 1;
+        for(int i = 1; i <= n; i++)
+        {
+            if((i % 2) == 1)
+            {
+                for(int j = 1; j <= n; j++)
+                {
+                    if((c % 2) == 1)
+                        ans[i][j] = sm++;
+                    else
+                        ans[i][j] = la--;
+                    c++;
+                }
+            }
+            else
+            {
+                for(int j = n; j >= 1; j--)
+                {
+                    if((c % 2) == 1)
+                        ans[i][j] = sm++;
+                    else
+                        ans[i][j] = la--;
+                    c++;
+                }
+            }
+        }
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= n; j++)
+            {
+                cout << ans[i][j] << " ";
+            }
+            cout << "\n";
+        }
+    }
+    return 0;
+}
