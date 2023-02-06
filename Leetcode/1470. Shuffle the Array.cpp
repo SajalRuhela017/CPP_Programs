6f44d0136ba590c0ec54f27c32e36f447f02da4e
SajalRuhela017 <ruhela.sajal017@gmail.com>
2023-02-06 18:05:02 +0530

    First Commit

---
+++ Leetcode/1470. Shuffle the Array.cpp
@@ -0,0 +1,17 @@
+// Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+
+// Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) 
+    {
+        vector<int> ans;
+        for(int i = 0, j = n; i < n; i++, j++)
+        {
+            ans.push_back(nums[i]);
+            ans.push_back(nums[j]);
+        }    
+        return ans;
+    }
+};
