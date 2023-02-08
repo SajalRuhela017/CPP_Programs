46f16556874010f6f9bdf675c7aa7cf729c4b0f9
SajalRuhela017 <ruhela.sajal017@gmail.com>
2023-02-09 01:58:57 +0530

    First Commit

--- Codeforces/Education Round #839/C. Different Differences.cpp
+++ Codeforces/Education Round #839/C. Different Differences.cpp
@@ -1,4 +1,8 @@
-#include <bitsstdc++.h>
+/**
+ *      author : rhsajal_017
+**/
+
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
@@ -10,8 +14,26 @@ int main()
     while(t--)
     {
         int n, k;
-        cin >> n >> k;
+        cin >> k >> n;
         vector<int> ans;
-        
+        ans.push_back(1);
+        for(int i = 1; i < k; i++)
+        {
+            int next = (i * i + i + 2) / 2, size = ans.size();
+            if(n - next >= k - size - 1)
+            {
+                ans.push_back(next);
+            }
+            else
+            {
+                int last = ans.back();
+                ans.push_back(++last);
+            }
+        }
+        for(auto i : ans)
+        {
+            cout << i << " ";
+        }
+        cout << endl;
     }
 }
