cfc876fb1b39e76fcdab2903e5386c12d2ee3eef
SajalRuhela017 <ruhela.sajal017@gmail.com>
2023-03-02 14:00:39 +0530

    First Commit

---
+++ Leetcode/443. String Compression.cpp
@@ -0,0 +1,45 @@
+// Given an array of characters chars, compress it using the following algorithm:
+
+// Begin with an empty string s. For each group of consecutive repeating characters in chars:
+
+// If the group's length is 1, append the character to s.
+// Otherwise, append the character followed by the group's length.
+// The compressed string s should not be returned separately, but instead, be stored in the input character array chars. Note that group lengths that are 10 or longer will be split into multiple characters in chars.
+
+// After you are done modifying the input array, return the new length of the array.
+
+// You must write an algorithm that uses only constant extra space.
+
+class Solution {
+public:
+    int compress(vector<char>& chars) 
+    {
+        string ans = "";
+        int count = 1;
+        for(int i = 1; i < chars.size(); i++)
+        {
+            if(chars[i] == chars[i - 1])
+                count++;
+            else
+            {
+                ans += chars[i - 1];
+                if(count > 1)
+                {
+                    string temp = to_string(count);
+                    ans += temp;
+                }
+                count = 1;
+            }
+        }
+        ans += chars.back();
+        if(count > 1)
+        {
+            string temp = to_string(count);
+            ans += temp;
+        }
+        chars.clear();
+        for(int i = 0; i < ans.size(); i++)
+            chars.push_back(ans[i]);
+        return ans.size();
+    }
+};
