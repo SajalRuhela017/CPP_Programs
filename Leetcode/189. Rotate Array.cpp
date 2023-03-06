dd4d472769f93e5f292c7d82ea86a8abc8b07724
SajalRuhela017 <ruhela.sajal017@gmail.com>
2023-03-06 22:39:14 +0530

    First Commit

--- Leetcode/189. Rotate Array.cpp
+++ Leetcode/189. Rotate Array.cpp
@@ -1,28 +1,15 @@
 // Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
 
-    class Solution 
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) 
     {
-    public:
-        void rotate(vector<int>& nums, int k) 
-        {
-            int n = nums.size();
-            if ((n == 0) || (k <= 0))
-                return;
-            
-            int cntRotated = 0, start = 0, curr = 0, numToBeRotated = nums[0], tmp = 0;
-            while (cntRotated < n)
-            {
-                do
-                {
-                    tmp = nums[(curr + k) % n];
-                    nums[(curr + k) % n] = numToBeRotated;
-                    numToBeRotated = tmp;
-                    curr = (curr + k) % n;
-                    cntRotated++;
-                } while (curr != start);
-                start++;
-                curr = start;
-                numToBeRotated = nums[curr];
-            }
-        }
-    };
+        int n = nums.size();
+        k %= n;
+        if(!k)
+            return;
+        reverse(nums.begin() , nums.begin() + n - k);
+        reverse(nums.begin() + n - k , nums.end());
+        reverse(nums.begin() , nums.end());
+    }
+};
